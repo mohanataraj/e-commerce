d3e08f3f7f485f21a9d985e72aade19899a5ecc2
Mohan Nataraj <6969451+mohanataraj@users.noreply.github.com>
2022-02-24 16:42:58 +0000

    updated menu-item to navigate withRouter v5 Router-dom. Further need to upgrade to Router-dom v6

--- react-course/crwn-clothing/src/App.js
+++ react-course/crwn-clothing/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE !!!</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exaact path='/shop/hats' component={HatsPage} />
+        </Switch>
+        </BrowserRouter>
+
     </div>
   );
 }
